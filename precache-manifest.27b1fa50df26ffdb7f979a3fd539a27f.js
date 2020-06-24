7e7ab3df9569635bf3dc765959585a1bfed3e64a
kafarzada <52622175+kafarzada@users.noreply.github.com>
2020-06-24 16:59:46 +0700

    Updates

---
+++ precache-manifest.27b1fa50df26ffdb7f979a3fd539a27f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e924f75d93ea59aeb7d9391767a1e64",
+    "url": "/ot_Ignata/index.html"
+  },
+  {
+    "revision": "8b9a82e9cc4e574dc5ed",
+    "url": "/ot_Ignata/static/css/main.91621d79.chunk.css"
+  },
+  {
+    "revision": "3f44109a799d2231dc7d",
+    "url": "/ot_Ignata/static/js/2.3881d28f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ot_Ignata/static/js/2.3881d28f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b9a82e9cc4e574dc5ed",
+    "url": "/ot_Ignata/static/js/main.cda7bcb2.chunk.js"
+  },
+  {
+    "revision": "4b697f86a286ae363e7c",
+    "url": "/ot_Ignata/static/js/runtime-main.8b9aa407.js"
+  }
+]);
